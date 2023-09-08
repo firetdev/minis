81cd1cce24f96b10e611e328ad379613592c1406
flesheatindragon <futuregamesstore@gmail.com>
2023-09-08 17:16:18 -0500

    queue ruby

---
+++ queue.rb
@@ -0,0 +1,33 @@
+class Queue
+	def initialize
+		@queue = []
+	end
+	def add user
+		@queue.push user
+	end
+	def print
+		puts @queue[0].id
+		@queue.delete_at(0)
+	end
+end
+
+class User
+	attr_accessor :id
+	def initialize
+		@id = rand(999999999999)
+	end
+end
+
+q = Queue.new
+
+puts "How many ids?"
+ids = gets.to_i
+
+(1..ids).each do |i|
+	i = User.new
+	q.add i
+end
+
+(1..ids).each do |x|
+	q.print
+end
